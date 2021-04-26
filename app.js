7623f5d65c045a583ef68284746cfc27da950419
LAURA JELENICH <ljelenich85@gmail.com>
2021-04-26 13:10:05 -0400

    new log

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
@@ -6,3 +7,4 @@ const msg = getNotes()
 console.log(msg)
 
 console.log(validator.isEmail('laura@gmail.com'))
+console.log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'))
