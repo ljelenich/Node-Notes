507e507f01724337cc7e6dde7b2759499459986b
LAURA JELENICH <ljelenich85@gmail.com>
2021-04-27 15:05:04 -0400

    ...

--- Notes.js
+++ Notes.js
@@ -1,33 +1,29 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = function () {
+const getNotes = () => {
     return 'Your notes'
 }
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
-
+    const duplicateNotes = notes.filter((note) => note.title === title)
     if (duplicateNotes.length === 0) {
         notes.push({
             title: title,
             body: body
         })
         saveNotes(notes)
+        console.log(chalk.green.inverse('Note Added!'))
     } else {
-        console.log('note title taken!')
+        console.log(chalk.red.inverse('No Note Added!'))
     }
     
 }
 
-const removeNote = function (title) {
+const removeNote = (title) => {
     const notes = loadNotes()
-    const notesToKeep = notes.filter(function (note) {
-        return note.title !== title
-    })
+    const notesToKeep = notes.filter((note) => note.title !== title)
 
     if (notes.length > notesToKeep.length) {
         console.log(chalk.green.inverse('Note Removed!'))
@@ -38,12 +34,12 @@ const removeNote = function (title) {
     saveNotes(notesToKeep)
 }
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJson = dataBuffer.toString()
--- app.js
+++ app.js
@@ -23,7 +23,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function(argv) {
+    handler(argv) {
         notes.addNote(argv.title, argv.body)
     }
 })
@@ -39,7 +39,7 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function (argv) {
+    handler(argv) {
         notes.removeNote(argv.title)
     }
 })
@@ -48,7 +48,7 @@ yargs.command({
 yargs.command({
     command: 'list', 
     describe: 'View list of notes',
-    handler: function() {
+    handler() {
         console.log('My List')
     }
 })
@@ -57,7 +57,7 @@ yargs.command({
 yargs.command({
     command: 'read', 
     describe: 'Read notes',
-    handler: function() {
+    handler() {
         console.log('Title: ' + argv.title)
         console.log('Body: ' + argv.body)
     }
